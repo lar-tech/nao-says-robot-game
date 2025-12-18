6be240197782a098c6c9d062586d48719e233c3f
Ramin Leon Neymeyer <ramin.leon.neymeyer@gmail.com>
2025-12-18 17:31:03 +0100

    restructed repo

--- lerngruppe_scripts/download_model.py
+++
@@ -1,7 +0,0 @@
-from huggingface_hub import snapshot_download
-
-repo_id = "facebook/s2t-small-librispeech-asr"
-local_path = '/Users/leon/Library/Mobile Documents/com~apple~CloudDocs/Uni/Master/3.Semester/naoqi-docker/project/nao-says-robot-game/models'
-
-local_dir = snapshot_download(repo_id, local_dir=local_path)
-print("Heruntergeladen nach:", local_dir)
