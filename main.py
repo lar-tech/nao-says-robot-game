54b3aaa6bd0fe14ad9e444b89655192ac0e37f2d
Ramin Leon Neymeyer <ramin.leon.neymeyer@gmail.com>
2025-12-18 17:31:41 +0100

    added inao to dependecy

--- main.py
+++ main.py
@@ -1,83 +1,33 @@
 import os
-import time
-import sys
-sys.path.insert(0, "/workspace/inao")
-CURRENT_DIR = os.path.dirname(os.path.abspath(__file__))
-SRC_DIR = os.path.join(CURRENT_DIR, "..", "src")
-sys.path.insert(0, SRC_DIR)
-from inao import NAO
-from naoqi import ALProxy
+import subprocess
 
-class NaoSays:
-    def __init__(self, ip, port):
-        self.tts = ALProxy("ALTextToSpeech", ip, port)
-        self.leds = ALProxy("ALLeds", ip, port)
-        
-    def change_eye_color(self, color):
-        color_map = {
-            "red": (1.0, 0.0, 0.0),
-            "green": (0.0, 1.0, 0.0),
-            "blue": (0.0, 0.0, 1.0),
-            "yellow": (1.0, 1.0, 0.0)
-            }
-        if color in color_map:
-            rgb = color_map[color]
-            self.leds.fadeRGB("FaceLeds", rgb[0], rgb[1], rgb[2], 0.5)
-        time.sleep(1)
-        self.leds.fadeRGB("FaceLeds", 1.0, 1.0, 1.0, 0.3)
-        time.sleep(0.3)
+from huggingface_hub import snapshot_download
 
-    # motion reaction
-    def posture(self, posture_name="stand", speed=1.0):
-        """
-        Set the robot in a given posture
-        :param posture_name: name of the posture
-        :param speed: speed of the movement
-        :return: None
-        """ 
-        ttsProxy = ALProxy("ALTextToSpeech", self.robotIP, self.PORT)
-        postureProxy = ALProxy("ALRobotPosture",self.robotIP, self.PORT)
-        motionProxy = ALProxy("ALMotion",self.robotIP, self.PORT)
-        postureProxy.goToPosture(posture_name, speed)
-        ttsProxy.say("Movement {} complete!".format(posture_name))
-        motionProxy.rest()
-
-    def move_position(self, x=0.0, y=0.0, theta=0.0):
-        """
-        Move the robot
-        :param x: forward/backward movement in meters
-        :param y: left/right movement in meters
-        :param theta: rotation in radians
-        :return: None
-        """
-        ttsProxy = ALProxy("ALTextToSpeech", self.robotIP, self.PORT)
-        motionProxy = ALProxy("ALMotion",self.robotIP, self.PORT)
-        motionProxy.wakeUp()
-        motionProxy.moveTo(x, y, theta)
-        ttsProxy.say("Movement complete!")
-        motionProxy.rest()
-
-    def move_joint(self, joint_name="HeadYaw", angle=0.0, speed=0.1, waitingtime=2.0):
-        """
-        Move a specific joint
-        :param joint_name: name of the joint
-        :param angle: angle in radians
-        :param speed: speed of the movement (0.0 to 1.0)
-        :return: None
-        """
-        ttsProxy = ALProxy("ALTextToSpeech", self.robotIP, self.PORT)
-        motionProxy = ALProxy("ALMotion",self.robotIP, self.PORT)
-        motionProxy.wakeUp()
-        motionProxy.setStiffnesses(joint_name, 1.0)
-        motionProxy.setAngles(joint_name, angle, speed)
-        ttsProxy.say("Movement of joint {} complete!".format(joint_name))
-        time.sleep(waitingtime)
-        motionProxy.setStiffnesses(joint_name, 0.0)
-        motionProxy.rest()
+from src.voice_commands import NaoVoiceCommand
+os.environ["TOKENIZERS_PARALLELISM"] = "false"
 
 if __name__ == "__main__":
+    # config
     ROBOT_IP = "192.168.1.118"
-    PORT = 9559
-    robot = NaoSays(ROBOT_IP, PORT)
-    robot.change_eye_color("yellow")
-    # game.record_audio(5)
+    PORT = "9559"
+    MODEL_DIR = "./models/qwen"
+    if not os.path.exists(MODEL_DIR):
+        local_dir = snapshot_download("Qwen/Qwen2.5-1.5B-Instruct", local_dir=MODEL_DIR, local_dir_use_symlinks=False)
+        print("Model downloaded to:", local_dir)
+
+    # get voice command
+    with NaoVoiceCommand() as recorder:
+        command = recorder.record_audio()
+
+    print(command)
+
+    # execute on robot
+    parent_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+    cmd = [parent_dir + "/run-naoqi.sh", "python2.7", "src/execute.py", ROBOT_IP, PORT, "Test"]
+    result = subprocess.run(cmd, capture_output=True, text=True, check=False)
+    
+    # results
+    print("returncode:", result.returncode)
+    print("stdout:\n", result.stdout)
+    print("stderr:\n", result.stderr)
+
